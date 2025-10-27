03692a1c666325cc8d5d88f495b95becb231e182
MyStic <khanh.votuan@hcmut.edu.vn>
2025-10-28 00:16:57 +0700

    feat(deploy): config

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,18 @@
 import dotenv from "dotenv";
 import { createApp } from "./app";
 
+import Koa from "koa";
+
 const startServer = async () => {
   try {
     dotenv.config();
 
-    await createApp();
+    const app: Koa = await createApp();
+
+    app.listen(8080);
   } catch (error) {
     console.log(error);
   }
 };
 
-export default startServer;
+startServer();
